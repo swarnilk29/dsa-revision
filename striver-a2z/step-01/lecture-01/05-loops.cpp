5f768ec4ab5725c7a1eec975cea5db2af28a7a90
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-05-04 00:21:08 +0530

    commit

---
+++ striver-a2z/step-01/lecture-01/05-loops.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    // for loop
+    for(int i = 1; i<10; i++){
+        cout << "google" << endl;
+    }
+
+    // while loop
+    int j;//initiation
+    while (j<=5) {//condition
+        cout << "wow" << endl;//print
+        j = j+1;//incrementation
+    }
+    return 0;
+
+    // do while
+    int k = 2;
+    do{
+        cout << "yahoo" << endl;
+    }while(k<=1);
+    cout << k << endl;
+}
