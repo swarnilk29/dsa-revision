972b55f07b543c1489ddeb2a47dd12ba4f4b5665
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-05-04 15:00:00 +0530

    commit

---
+++ step-01/lecture-01/06-functions.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Functions are set of code which performs something for you
+//Functions are used to modularise code
+//Functions are used to increase readability
+//Functions are used to use the same code multiple time
+//void -> does not return anything
+//parameterised
+//non-parameterised
+
+void printName(string name){
+    cout << "Hello " << name << endl;
+}
+
+int main (){
+    string name;
+    cin >> name;
+    printName(name);
+    
+
+    string name2;
+    cin >> name2;
+    printName(name2);
+    return 0;
+}
+
+// question - Take two numbers and print its sum
+
+void sum (){
+    
+}
+
+int main (){
+    
+}
