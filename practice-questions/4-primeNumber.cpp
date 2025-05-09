17697e4b37c7169a4fda53366f327f31f7faac54
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-05-09 23:57:09 +0530

    commit

--- practice-questions/4-primeNumber.cpp
+++ practice-questions/4-primeNumber.cpp
@@ -1,24 +1,52 @@
-// code testing file
+// // code testing file
 
 
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main (){
+//     int n;
+//     cout << "Enter number : ";
+//     cin >> n;
+
+//     int i = 2;
+
+//     while (i<n){
+//         if (n%i == 0){
+//             cout << "Not a Prime Number";
+//         }
+//         else {
+//             cout << "It is a Prime Number";
+//         }
+        
+//     }
+
+// }
+
+
+#include <iostream>
 using namespace std;
 
-int main (){
-    int n;
-    cout << "Enter number : ";
+int main() {
+    int n, count = 0;
     cin >> n;
 
-    int i = 2;
-
-    while (i<n){
-        if (n%i == 0){
-            cout << "Not a Prime Number";
-        }
-        else {
-            cout << "It is a Prime Number";
-        }
-        
+    if (n <= 1) {
+        cout << "Not Prime";
+        return 0;
     }
 
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            count++;
+            break;
+        }
+    }
+
+    if (count == 0)
+        cout << "Prime";
+    else
+        cout << "Not Prime";
+
+    return 0;
 }
