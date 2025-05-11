b16b3cd466f7f0423ffce7b62f6cec0189a5691f
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-05-11 23:21:16 +0530

    commit

--- practice-questions/0-demo.cpp
+++ practice-questions/0-demo.cpp
@@ -2,19 +2,3 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void Pattern1(int n){
-    for (int i = 0; i < n ; i++){
-        for (int j = 0; j < n; j++){
-            cout << " * " ;
-        }
-        cout << endl;
-    }
-}
-
-
-int main (){
-    int n;
-    cin >> n;
-    Pattern1(n);
-    return 0;
-}
